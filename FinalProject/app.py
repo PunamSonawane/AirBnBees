e6661f573eaf31761ff5d9d967a5e4b7f2a367ac
PunamSonawane <sonawanepunam27@gmail.com>
2020-03-27 17:23:14 -0400

    friday commit

--- FinalProject/app.py
+++ FinalProject/app.py
@@ -1,3 +1,6 @@
+# Dependencies
+import pandas as pd
+
 from flask import (
     Flask,
     render_template,
@@ -22,10 +25,12 @@ def dash():
 
 @app.route("/model")
 def model():
-    return render_template("dashboard.html")
+    neb_df = pd.read_csv("static/data/sub.csv", encoding="utf-8")
+    nbh=neb_df['neighbourhood_cleansed'].unique()
+    return render_template("form.html",test=nbh)
 
 @app.route("/map")
 def map():
-    return render_template("dashboard.html")
+    return render_template("map.html")
 if __name__ == "__main__":
     app.run(debug=True)
